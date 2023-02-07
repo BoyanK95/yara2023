dd08f898f763b2dcd88877a4d35511eed3ded26c
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-02-07 21:44:50 +0200

    Added config json file to the projects

--- Section-2-JS/udemy-JS-course/section3/reviewingFunctions.js
+++ Section-2-JS/udemy-JS-course/section3/reviewingFunctions.js
@@ -1,8 +1,17 @@
 const yearUntilRetirement = (birthYear, firstName) => {
-    const age = 2023 - birthYear
-    const retirement = 65 - age
+    const age = 2023 - birthYear;
+    const retirement = 65 - age;
 
-    return `${firstName} is going to retire in ${retirement} years`
-}
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} , ha already retired 👨🏻‍🦳`);
+        return -1;
+    }
+
+    // return `${firstName} is going to retire in ${retirement} years`
+};
 
 console.log(yearUntilRetirement(1995, 'Boyan'));
+console.log(yearUntilRetirement(1952, 'Stoyan'));
