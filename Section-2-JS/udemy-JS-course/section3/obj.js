31d8e067eeb143bf206f8a95e2d97a99e73acf51
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-02-08 12:18:26 +0200

    Create obj.js

---
+++ Section-2-JS/udemy-JS-course/section3/obj.js
@@ -0,0 +1,18 @@
+const coder = {
+    firstName: 'Boyan',
+    lastName: 'Koychev',
+    age: 2023 - 1995,
+    bornInCity: 'Bourgas',
+    livesInCity: 'Varna',
+    curCity: 'Dimitrovgrad',
+    firends: ['Paulin', 'Kraso', 'Denis']
+}
+
+console.log(coder);
+
+
+//Dot notatiom
+console.log(coder.firstName);
+
+//Bracet notion
+console.log(coder['firstName']);
