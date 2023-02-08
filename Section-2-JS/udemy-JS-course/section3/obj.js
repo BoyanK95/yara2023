4c20811a6678ca1497b8ad1a73832d948cf87973
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-02-08 16:23:38 +0200

    A little working with object in the Dom

--- Section-2-JS/udemy-JS-course/section3/obj.js
+++ Section-2-JS/udemy-JS-course/section3/obj.js
@@ -6,13 +6,25 @@ const coder = {
     livesInCity: 'Varna',
     curCity: 'Dimitrovgrad',
     firends: ['Paulin', 'Kraso', 'Denis']
-}
+};
 
 console.log(coder);
-
 
 //Dot notatiom
 console.log(coder.firstName);
 
 //Bracet notion
 console.log(coder['firstName']);
+
+const nameKey = 'Name';
+
+console.log(coder['first' + nameKey]);
+console.log(coder['last' + nameKey]);
+
+const interestedIn = prompt(
+    'What do you want to know about me? Chose between firstName, lastName, age, bornIncity, liveInCity, curCity, and friends'
+);
+
+console.log(interestedIn);
+
+console.log(coder[interestedIn]);
