afae85fcc8d0e1d58d70587a59a7b2cca1d0be07
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-02-09 11:22:42 +0200

    Created a Eval solution!
    Finished Eval solution, which is the easiest to create the logic for. Added L to the layout btn for easiest target

--- Section-2-JS/JS-Calculator-exercise/calculatorApp.js
+++ Section-2-JS/JS-Calculator-exercise/calculatorApp.js
@@ -1,12 +1,27 @@
-const display = document.getElementById('display')
-const value = document.getElementById('value')
-const buttons = document.querySelectorAll('button')
-const layoutBtn = document.getElementById('toggleBtn')
-const body = document.querySelector('body')
+// const display = document.getElementById('display');
+const value = document.getElementById('value');
+const buttons = document.querySelectorAll('button');
+// const layoutBtn = document.getElementById('toggleBtn');
+const body = document.querySelector('body');
 
 
-
-// Toggle Button
-layoutBtn.addEventListener('click', () => {
-    body.classList.toggle('dark')
-})
+//Eval solution !
+buttons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        if (btn.innerText === 'L') {
+            body.classList.toggle('dark');
+        } else if (btn.innerText === '=') {
+            //Easiest solution is with eval, not correct though, cause it renders us easy for Malicious code (hack attack)!
+            value.innerHTML = eval(value.innerHTML);
+        } else if (btn.innerText === 'C') {
+            const newValue = Array.from(value.innerHTML).slice(0, value.innerHTML.length - 1);
+            value.innerHTML = newValue.join('')
+        } else {
+            if (btn.innerHTML === 'CA') {
+                value.innerHTML = '';
+            } else {
+                value.innerHTML += btn.innerHTML;
+            }
+        }
+    });
+});
