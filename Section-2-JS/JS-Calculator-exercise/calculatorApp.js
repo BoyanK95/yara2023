f1fc2400374917bdd80b39f8f738c68389ed141e
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-02-09 09:48:04 +0200

    Finished css styling and layout toggle button

--- Section-2-JS/JS-Calculator-exercise/calculatorApp.js
+++ Section-2-JS/JS-Calculator-exercise/calculatorApp.js
@@ -1,1 +1,12 @@
-console.log('Test!!');
+const display = document.getElementById('display')
+const value = document.getElementById('value')
+const buttons = document.querySelectorAll('button')
+const layoutBtn = document.getElementById('toggleBtn')
+const body = document.querySelector('body')
+
+
+
+// Toggle Button
+layoutBtn.addEventListener('click', () => {
+    body.classList.toggle('dark')
+})
