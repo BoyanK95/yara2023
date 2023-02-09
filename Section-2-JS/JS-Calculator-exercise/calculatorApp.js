f1995aaa62dcc56f4bebfc9b897c23028dab036c
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-02-09 16:20:26 +0200

    Added error handling in the evalIsEvil function
    Added error handling in the evalIsEvil function, so that it can check for the length of the result and display error if the number are more than the max 8 given in the exercise description

--- Section-2-JS/JS-Calculator-exercise/calculatorApp.js
+++ Section-2-JS/JS-Calculator-exercise/calculatorApp.js
@@ -7,7 +7,7 @@ buttons.forEach((btn) => {
         if (btn.innerText === 'L') {
             body.classList.toggle('dark');
         } else if (btn.innerText === '=') {
-            //Easiest solution is with eval, not correct though, cause it renders us easy for Malicious code (hack attack)!
+            //Easiest solution is with eval, not correct though, cause it renders us easy for Malicious code!
             // value.innerHTML = eval(value.innerHTML);
             value.innerHTML = evalIsEvil(value.innerHTML);
         } else if (btn.innerText === 'C') {
@@ -15,8 +15,10 @@ buttons.forEach((btn) => {
             value.innerHTML = newValue.join('');
         } else if (btn.innerHTML === 'CA') {
             value.innerHTML = '';
-        } else {
+        } else if (value.innerHTML.length < 8){
             value.innerHTML += btn.innerHTML;
+        } else {
+            value.innerHTML = 'ERR'
         }
     });
 });
@@ -26,24 +28,61 @@ function evalIsEvil(input) {
         const firstNum = input.split('+')[0];
         const secondNum = input.split('+')[1];
         result = Number(firstNum) + Number(secondNum);
-        return isNaN(result) ? 'ERR' : result;
+        console.log(Array.from(result.toString()).length);
+        if (Array.from(result.toString()).length < 8) {
+            return isNaN(result) ? 'ERR' : result;
+        } else if (Array.from(result.toString()).length >= 8){
+            console.log(Array.from(result.toString()));
+            return 'ERR'
+        }
     }
     if (input.includes('-')) {
         const firstNum = input.split('-')[0];
         const secondNum = input.split('-')[1];
         result = Number(firstNum) - Number(secondNum);
-        return isNaN(result) ? 'ERR' : result;
+        console.log(Array.from(result.toString()).length);
+        if (Array.from(result.toString()).length < 8) {
+            return isNaN(result) ? 'ERR' : result;
+        } else if (Array.from(result.toString()).length >= 8){
+            console.log(Array.from(result.toString()));
+            return 'ERR'
+        }
     }
     if (input.includes('*')) {
         const firstNum = input.split('*')[0];
         const secondNum = input.split('*')[1];
         result = Number(firstNum) * Number(secondNum);
-        return isNaN(result) ? 'ERR' : result;
+        console.log(Array.from(result.toString()).length);
+        if (Array.from(result.toString()).length < 8) {
+            return isNaN(result) ? 'ERR' : result;
+        } else if (Array.from(result.toString()).length >= 8){
+            console.log(Array.from(result.toString()));
+            return 'ERR'
+        }
     }
     if (input.includes('/')) {
         const firstNum = input.split('/')[0];
         const secondNum = input.split('/')[1];
         result = Number(firstNum) / Number(secondNum);
-        return isNaN(result) ? 'ERR' : result;
+        console.log(Array.from(result.toString()).length);
+        if (Array.from(result.toString()).length < 8) {
+            return isNaN(result) ? 'ERR' : result;
+        } else if (Array.from(result.toString()).length >= 8){
+            console.log(Array.from(result.toString()));
+            return 'ERR'
+        }
     }
 }
+
+//In a real project I would use JavaScript Expression Evaluator package
+// Installation: npm install expr-eval
+
+// Basic Usage:
+// var Parser = require('expr-eval').Parser;
+
+// var parser = new Parser();
+// var expr = parser.parse('2 * x + 1');
+// console.log(expr.evaluate({ x: 3 })); // 7
+
+// // or
+// Parser.evaluate('6 * x', { x: 7 }) // 42
