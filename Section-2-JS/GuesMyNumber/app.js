7c2bc24491bffcfe8390c03ac7b23180f3db64cb
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-02-10 22:44:37 +0200

    Refactored the code a little bit

--- Section-2-JS/GuesMyNumber/app.js
+++ Section-2-JS/GuesMyNumber/app.js
@@ -1,66 +1,67 @@
-'use strict'
+'use strict';
 
-const body = document.querySelector('body')
-const number = document.getElementsByClassName('number')[0]
-const guess = document.getElementsByClassName('guess')[0]
-const checkBtn = document.getElementsByClassName('check')[0]
-const againBtn = document.getElementsByClassName('again')[0]
-const message = document.querySelector('.message')
-const buttons = document.querySelectorAll('button')
-let scoreText = document.querySelector('.score')
+const body = document.querySelector('body');
+const number = document.getElementsByClassName('number')[0];
+const guess = document.getElementsByClassName('guess')[0];
+const checkBtn = document.getElementsByClassName('check')[0];
+const againBtn = document.getElementsByClassName('again')[0];
+const message = document.querySelector('.message');
+const buttons = document.querySelectorAll('button');
+const highScore = document.getElementsByClassName('highscore')[0];
+let scoreText = document.querySelector('.score');
 
-let randomNumber = Number(Math.ceil(Math.random() * 20))
+let randomNumber = Number(Math.ceil(Math.random() * 20));
 console.log(randomNumber);
 
 function addEffect() {
-    let score = Number(scoreText.textContent)
+    let score = Number(scoreText.textContent);
     console.log(Number(guess.value));
     console.log(score);
-    
-    if (guess.value == '') {
-        message.textContent = '⛔️ No Number!'
-        score -= 1
-        scoreText.textContent = score
-        return
-    }
-    let result = compareNumbers(Number(guess.value), randomNumber)
 
-    if (result === true) {
-        body.style.backgroundColor = '#60b347'
-        message.textContent = `🎉 Congratulation you WIN !` 
-        return
-    } else if (result === 'bigger') {
-        score -= 1
-        scoreText.textContent = score
-        message.textContent = 'Your number is bigger'
-        return
-    } else if (result === 'smaller') {
-        score -= 1
-        scoreText.textContent = score
-        message.textContent = 'Your number is smaller'
-        return
+    if (guess.value == '') {
+        message.textContent = '⛔️ No Number!';
+        score -= 1;
+        scoreText.textContent = score;
+        return;
+    }
+    compareNumbers(Number(guess.value), randomNumber, score);
+}
+
+buttons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        if (btn.className === 'btn check') {
+            addEffect();
+        } else if (btn.className == '') {
+        }
+    });
+});
+
+function compareNumbers(guessNum, originalNum, score) {
+    if (guessNum == originalNum) {
+        body.style.backgroundColor = '#60b347';
+        message.textContent = `🎉 Congratulation you WIN !`;
+        highScoreSetter(score);
+        return;
+    } else if (guessNum > originalNum) {
+        score -= 1;
+        scoreText.textContent = score;
+        message.textContent = 'Chose a smaller number';
+        return;
+    } else if (guessNum < originalNum) {
+        score -= 1;
+        scoreText.textContent = score;
+        message.textContent = 'Chose a bigger number!';
+        return;
     }
 }
 
-buttons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        if (btn.className === 'btn check') {
-            addEffect()  
-       } else if (btn.className == '') {
-           
-       }
-    })
-});
+function highScoreSetter(currentScore) {
+    let bestScore = localStorage.getItem('bestScore');
     
-    
-
-
-function compareNumbers(guessNum, originalNum) {
-    if (guessNum == originalNum) {
-        return true
-    } else if (guessNum > originalNum) {
-        return 'bigger'
-    } else if (guessNum < originalNum) {
-        return 'smaller'
+    if (!bestScore) {
+        bestScore = Number(highScore.textContent);
     }
+    currentScore > bestScore
+        ? localStorage.setItem('bestScore', currentScore)
+        : localStorage.setItem('bestScore', bestScore);
 }
