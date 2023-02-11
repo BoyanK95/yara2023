4be36616d3265acca471b38dfd0470a9fdcf23de
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-02-11 18:43:59 +0200

    Created closeModal function and finished exercise

--- Section-2-JS/udemy-JS-course/section7/ModalWindow/app.js
+++ Section-2-JS/udemy-JS-course/section7/ModalWindow/app.js
@@ -1,17 +1,23 @@
-'use strict'
+'use strict';
 
-const  showModalBtn = document.querySelectorAll('.show-modal')
-const closeModalBtn = document.querySelector('.close-modal')
-const modal = document.getElementsByClassName('modal')[0]
-const overlay = document.querySelector('.overlay')
+const showModalBtn = document.querySelectorAll('.show-modal');
+const closeModalBtn = document.querySelector('.close-modal');
+const modal = document.getElementsByClassName('modal')[0];
+const overlay = document.querySelector('.overlay');
 
-console.log(overlay);
+showModalBtn.forEach((btn) => {
+    btn.addEventListener('click', showModal);
+});
 
-showModalBtn.forEach(btn => {
-    btn.addEventListener('click', showModal)
-})
+closeModalBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+function closeModal() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
 
 function showModal() {
-    modal.classList.remove('hidden')
-    overlay.classList.remove('hidden')
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
 }
