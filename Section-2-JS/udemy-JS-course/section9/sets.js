becb55101f9e09310ef11b3e545b33b8d1baf35b
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-02-13 09:15:38 +0200

    Little working with sets

--- Section-2-JS/udemy-JS-course/section9/sets.js
+++ Section-2-JS/udemy-JS-course/section9/sets.js
@@ -1,1 +1,25 @@
- 
+const orderSet = new Set(['Pasta', 'Pizza', 'Pasta', 'Risotto', 'Pasta', 'Pizza', 'Risotto']);
+
+console.log(orderSet);
+console.log(new Set('Paulin'));
+console.log(orderSet.size);
+console.log(orderSet.has('Pizza'));
+console.log(orderSet.has('Bread'));
+orderSet.add('Garlic Bread');
+orderSet.add('Garlic Bread');
+orderSet.delete('Risotto');
+console.log(orderSet);
+
+for (const order of orderSet) {
+    console.log(order);
+}
+
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUniqie = [...new Set(staff)]
+console.log(staff);
+console.log(staffUniqie);
+
+//For counting how manny different values an arr has
+console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size);
+
+console.log(new Set('BoyanKoychev').size);
