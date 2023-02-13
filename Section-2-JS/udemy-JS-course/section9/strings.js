d8ec5ce44a4ac84116946c9647855082381ca307
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-02-13 10:48:12 +0200

    Little exercise with strings

---
+++ Section-2-JS/udemy-JS-course/section9/strings.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const airline = 'Qatar AIR'
+const plane = 'Boeing 777'
+console.log(plane[0]);
+console.log(plane[1]);
+
+console.log(airline.indexOf('a'))
+console.log(airline.lastIndexOf('a'))
+console.log(airline.indexOf('AIR'))
+
+//First word
+console.log(airline.slice(0, airline.indexOf(' ')));
+//Last word
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+//Split words in Arr
+console.log(airline.split(' '));
+
+
+
+const checkMiddleSeat = function (seat) {
+    /** B and E are middle seats! */
+    const s = seat.slice(-1);
+
+    if (s === 'B' || s === 'E') {
+        console.log('You got the middle seat😫');
+    } else {
+        console.log('You got lucky 😎');
+    }
+};
+
+// checkMiddleSeat('11B');
+// checkMiddleSeat('23C');
+// checkMiddleSeat('3E');
