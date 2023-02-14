19d3edc5db20591562d3c78f941c158212fef5ea
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-02-14 09:55:22 +0200

    Refreshing on objects and classes

---
+++ Section-2-JS/udemy-JS-course/section14/classes.js
@@ -0,0 +1,72 @@
+'use strict'
+
+//class expression
+const Dog = class {
+    constructor(firstName, birthYear, breed){
+        this.firstName = firstName
+        this.birthYear = birthYear
+        this.breed = breed
+    }
+
+    calcAge() {
+        console.log(2023 - this.birthYear);
+    }
+
+    bark() {
+        if (this.breed === 'pug') {
+            console.log(`Hurr, Hurr I'm a fat pug named ${this.firstName}`);
+        } else {
+            console.log(`WoooF, Wooof, I'm ${this.firstName}`);
+        }
+    }
+}
+
+//class declaration
+class Cat {
+    constructor(firstName, birthYear) {
+        this.firstName = firstName
+        this.birthYear = birthYear
+    }
+
+    calcAge() {
+        console.log(2023 - this.birthYear);
+    }
+
+    get age() {
+        return 2023 - this.birthYear
+    }
+}
+
+const ares = new Cat('Ares', 2002,)
+const kitrie = new Cat('Kitrie', 2007)
+console.log(ares);
+ares.calcAge()
+
+Cat.prototype.greet = function () {
+    console.log(`Meooow, my name is ${this.firstName}!`);
+}
+
+ares.greet()
+console.log(ares.age)
+kitrie.greet()
+
+const pug = new Dog('Aria', 1999, 'pug')
+pug.bark()
+
+const account = {
+    owner: 'Jamie',
+    movements: [200, 530, 120, 300],
+
+    get latest() {
+        return this.movements.slice(-1).pop()
+    },
+
+    set latest(mov) {
+        this.movements.push(mov)
+    }
+}
+
+console.log(account.latest);
+
+account.latest = 50
+console.log(account.movements);
--- Section-2-JS/udemy-JS-course/section14/script.js
+++ Section-2-JS/udemy-JS-course/section14/script.js
@@ -1,24 +1,52 @@
 'use strict';
 
 const Person = function (firstName, birthYear, job, weight, attackDmg, HP) {
-    this.firstName = firstName
-    this.birthYear = birthYear
-    this.job = job
-    this.weight = weight
-    this.attackDmg = attackDmg
-    this.HP = HP
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+    this.job = job;
+    this.weight = weight;
+    this.attackDmg = attackDmg;
+    this.HP = HP;
 
     //Never create a method in constructor function
-    this.attack = function () {
-        console.log(`${this.firstName} (with ${this.HP} HP) attacked target for ${this.attackDmg} DMG!`);
-    }
+    // this.attack = function () {
+    //     console.log(`${this.firstName} (with ${this.HP} HP) attacked target for ${this.attackDmg} DMG!`);
+    // }
 };
 
 const jonas = new Person('Jonas', 1991);
-const bobi = new Person('Bobi', 1997,'Wreastler', 86, 99, 100);
+const bobi = new Person('Bobi', 1997, 'Wreastler', 86, 99, 100);
 const stunio = new Person('Stunio', 1997, 'Custumer Sup', 75, 65, 100);
 
 console.log(bobi, stunio, jonas);
 
-bobi.attack()
- 
+console.log(Person.prototype);
+
+Person.prototype.attack = function () {
+    console.log(`${this.firstName} (with ${this.HP} HP) attacked target for ${this.attackDmg} DMG!`);
+};
+
+bobi.attack();
+
+/** Lection 2 */
+console.log('Lection 2');
+
+console.log(stunio.hasOwnProperty('firstName'));
+console.log(stunio.hasOwnProperty('species'));
+
+console.log(bobi.__proto__);
+console.log(bobi.__proto__.__proto__);
+console.log(bobi.__proto__.__proto__.__proto__);
+
+console.log('Arr');
+const arr = [3, 4, 5, 7, 8, 7, 7, 1, 3]
+console.log(arr.__proto__ );
+console.log(arr.__proto__.__proto__ );
+console.log(arr.__proto__.__proto__ );
+
+//Avoid doing this, might be problematic in future
+Array.prototype.unique = function () {
+    return [...new Set(this)]
+}
+
+console.log(arr.unique());
