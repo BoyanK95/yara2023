f51c311bc76d8e539958c42830d672142b97e1cf
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-02-16 16:44:03 +0200

    Set up and finished first toUpperCase exercise

---
+++ Lection 3JS/lectureNotes2/notes.js
@@ -0,0 +1,12 @@
+//SOLID Principles DO more research on it
+
+//1 Sole responcibility
+//2. Open-Close- obj should be open for extension but close to modification
+//3. Liskov principles
+//4. interface segregation - creating a child class, should never be forced to require methods that are not neccesary for that class
+//5.Dependacy inversion
+
+
+//Singleton Pattern
+//Prototype Pattern
+//Module Pattern
