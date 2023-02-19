6ddef807e14df2e4b5f94972a1ac7ba5db53cc4d
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-02-19 19:42:29 +0200

    Set up all dependance for fortune_api

--- node_postgres/first_server/index.js
+++ node_postgres/first_server/index.js
@@ -4,9 +4,15 @@ const hostname = 'localhost'
 const port = 3000
 
 const server = http.createServer((req, res) => {
+    const { url } = req
+    console.log(url);
+
+    // if (url === '/translations') {
+    //     res.end('translations')
+    // }
     res.end('Welcome to node!')
 })
 
 server.listen(port, hostname, () => {
     console.log(`Server running at ${hostname}:${port}`);
-})
+}) 
