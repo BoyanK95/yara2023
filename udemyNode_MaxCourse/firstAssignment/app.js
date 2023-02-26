959d7fee69d6d4ceb6927b4173a94fc5000fabc1
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-02-27 00:24:24 +0200

    Added a form with a button to send a POST req with the username added

--- udemyNode_MaxCourse/firstAssignment/app.js
+++ udemyNode_MaxCourse/firstAssignment/app.js
@@ -3,6 +3,7 @@ const port = 3000
 
 const server = http.createServer((req, res) => {
     const url = req.url
+    const method = req.method
     
     if (url === '/users') {
         res.setHeader('Content-Type', 'text/html')
@@ -17,9 +18,25 @@ const server = http.createServer((req, res) => {
         res.setHeader('Content-Type', 'text/html')
         res.write('<html>')
         res.write('<head><title>Greeting</title></head>')
-        res.write('<body><h1>Hello, It\'s nice to meet you!</h1></body>')
+        res.write('<body>')
+        res.write('<h1>Hello, It\'s nice to meet you!</h1>')
+        res.write('<form action="/create-user" method="POST"><input type="text" name="username" type="submit"><button>Add user</button></form>')
+        res.write('</body>')
         res.write('</html>')
         return res.end()
+    }
+    if (url === '/create-user' && method === 'POST') {
+        const body = []
+        req.on('data', (chunk) => {
+            body.push(chunk)
+        })
+        req.on('end', () => {
+            const parseBody = Buffer.concat(body).toString() //username=whatever=the-user-entered
+            console.log(parseBody.split('=')[1]);
+        })
+        res.statusCode = 302
+        res.setHeader('Location', '/')
+        res.end()
     }
 })
 
