30d81c8c77657fb24cca6f378b2dd36944147b29
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-02-27 20:26:24 +0200

    Created page-not-found.html and connected route to it

--- udemyNode_MaxCourse/Section5Express/workingWithExpress/app.js
+++ udemyNode_MaxCourse/Section5Express/workingWithExpress/app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+
+const path = require('path')
+
 const bodyParser = require('body-parser')
 
 const adminRoutes = require('./routes/admin')
@@ -12,7 +15,7 @@ app.use(adminRoutes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found!</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'))
 })
 
 app.listen(3000);
