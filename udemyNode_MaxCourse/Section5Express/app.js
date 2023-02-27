51da1da31bd32fa0de9450f088b250f4664f15ad
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-02-27 11:52:34 +0200

    Finished 2nd assignment for express udemy course

--- udemyNode_MaxCourse/Section5Express/app.js
+++ udemyNode_MaxCourse/Section5Express/app.js
@@ -1,15 +1,26 @@
 const express = require('express');
 
 const app = express();
+/**First part */
+// app.use((req, res, next) => {
+//     console.log('First Middleware');
+//     next();
+// });
 
-app.use((req, res, next) => {
-    console.log('First Middleware');
-    next();
+// app.use((req, res, next) => {
+//     console.log('Second Middleware!');
+//     res.send('<p>Assignment solved (almost)</p>')
+// });
+
+/**Second part */
+app.use('/user', (req, res, next) => {
+    console.log('Middleware users');
+    res.send('<h1>Users middleware</h1>');
 });
 
-app.use((req, res, next) => {
-    console.log('Second Middleware!');
-    res.send('<p>Assignment solved (almost)</p>')
+app.use('/', (req, res, next) => {
+    console.log('Normal middleware');
+    res.send('<h1>Normal middleware!</h1>')
 });
 
 app.listen(3000);
