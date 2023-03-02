a5a0c51aca513911388c1fdc5b2f83bd8412243d
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-03-02 17:51:01 +0200

    Displayed product on path: products
    Displayed product obj from inventorySaaS DB using shop controller

--- Lection_Node_Assignment/util/database.js
+++ Lection_Node_Assignment/util/database.js
@@ -7,16 +7,4 @@ const pool = new Pool({
   port: 5432,
 });
 
-const getProducts = (req, res ) => {
-    pool.query('SELECT * FROM product', (err, result) => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log(result);
-    })
-}
-
-getProducts()
-
-
 module.exports = pool
