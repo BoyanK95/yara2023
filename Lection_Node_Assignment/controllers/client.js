67b82503066c04b7e210670c4b598e07eddf9028
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-03-04 15:43:25 +0200

    Added clients view, and getClients functionality

---
+++ Lection_Node_Assignment/controllers/client.js
@@ -0,0 +1,29 @@
+const Client = require('../models/client')
+
+exports.getClient = (req, res, next) => {
+    Client.findAll()
+        .then((clients) => {
+            res.render('admin/clients', {
+                data: clients,
+                pageTitle: 'Clients',
+                path: '/admin/clients'
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.redirect('/');
+        });
+};
+
+// exports.postDeleteProduct = (req, res, next) => {
+//     const prodId = req.body.productId;
+//     Client.findByPk(prodId)
+//         .then((product) => {
+//           return product.destroy()
+//         })
+//         .then(result => {
+//           console.log('PRODUCT DESTROYED !!!');
+//           res.redirect('/admin/clients');
+//         })
+//         .catch(err => console.log(err));
+// };
--- Lection_Node_Assignment/routes/admin.js
+++ Lection_Node_Assignment/routes/admin.js
@@ -3,22 +3,22 @@ const path = require('path');
 const express = require('express');
 
 const adminController = require('../controllers/admin');
+const clientController = require('../controllers/client')
 
 const router = express.Router();
-
-// /admin/add-product => GET
+//Get products/add products, get edit products
 router.get('/add-product', adminController.getAddProduct);
-
-// /admin/products => GET
 router.get('/products', adminController.getProducts);
 
-// /admin/add-product => POST
-router.post('/add-product', adminController.postAddProduct);
+//Get clients routes
+router.get('/clients', clientController.getClient)
 
 router.get('/edit-product/:productId', adminController.getEditProduct);
 
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 router.post('/edit-product', adminController.postEditProduct);
-
+//Deleting products
 router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
