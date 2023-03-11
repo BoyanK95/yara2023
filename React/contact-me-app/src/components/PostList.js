c9d4f2af69248914420b79d99fbd4d40f094645d
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-03-12 00:00:04 +0200

    Created NewPost component and connected it to PostList

--- React/contact-me-app/src/components/PostList.js
+++ React/contact-me-app/src/components/PostList.js
@@ -1,12 +1,16 @@
+import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostList.module.css'
 
 function PostList() {
     return (
+        <>
+        <NewPost />
         <ul className={classes.posts}>
             <Post author='Boyan' body='React.js is fucking AWSOME!!!' />
             <Post author='Boyan' body='React.js is fucking AWSOME!!!' />
         </ul>
+        </>
     );
 }
 
