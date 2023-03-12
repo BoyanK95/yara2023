b0fc9a941d1b79ab52ac29ab7d58071046ca4072
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-03-12 02:59:57 +0200

    Created Modal component to hold NewPost

--- React/contact-me-app/src/components/PostList.js
+++ React/contact-me-app/src/components/PostList.js
@@ -1,22 +1,33 @@
 import { useState } from 'react';
+import Modal from './Modal';
 import NewPost from './NewPost';
 import Post from './Post';
 import classes from './PostList.module.css';
 
 function PostList() {
+    const [modalIsVisible, setModalIsVisible] = useState(true);
     const [enteredBody, setEnteredBody] = useState('');
     const [enteredAuthor, setEnteredAuthor] = useState('');
 
+    function hideModalHandler() {
+        setModalIsVisible(false);
+    }
+
     function bodyChangeHandler(e) {
-        setEnteredBody(e.target.value)
+        setEnteredBody(e.target.value);
     }
 
     function authorChangeHandler(e) {
-        setEnteredAuthor(e.target.value)
+        setEnteredAuthor(e.target.value);
     }
+
     return (
         <>
-            <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
+            {modalIsVisible ? (
+                <Modal onClose={hideModalHandler}>
+                    <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+                </Modal>
+            ) : null}
             <ul className={classes.posts}>
                 <Post author={enteredAuthor} body={enteredBody} />
                 <Post author='Boyan' body='React.js is fucking AWSOME!!!' />
