58feab36ff1d1b3fa3d6c06dd6da4e79d41b078c
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-03-13 18:23:16 +0200

    Created initial architecture. Logo and Header components

---
+++ React/ReactAssignment/yara-react-assignment/src/components/TopHeader/TopHeader.js
@@ -0,0 +1,12 @@
+import LogoImg from "../Logo/LogoImg";
+
+const TopHeader = () => {
+    return (
+        <header>
+            <LogoImg />
+            <div></div>
+        </header>
+    );
+};
+
+export default TopHeader
