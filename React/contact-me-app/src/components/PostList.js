863a8ba24f8fd0f7bac2c54b593797555d61c103
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-03-13 02:20:07 +0200

    Used useEffect to display posts, handled loading and error state

--- React/contact-me-app/src/components/PostList.js
+++ React/contact-me-app/src/components/PostList.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Modal from './Modal';
 import NewPost from './NewPost';
 import Post from './Post';
@@ -6,6 +6,29 @@ import classes from './PostList.module.css';
 
 function PostList({ isPosting, onStopPosting }) {
     const [posts, setPosts] = useState([]);
+    const [isFetching, setIsFetching] = useState(false);
+    const [hasError, setHasError] = useState(false);
+
+    useEffect(() => {
+        async function fetchPost() {
+            try {
+                setIsFetching(true);
+                const responce = await fetch('http://localhost:8080/posts');
+                const resData = await responce.json();
+                if (!resData) {
+                    setHasError(true);
+                    setIsFetching(false);
+                }
+                setPosts(resData.posts);
+                setIsFetching(false);
+            } catch (error) {
+                setIsFetching(false);
+                setHasError(true);
+                console.log(error);
+            }
+        }
+        fetchPost();
+    }, []);
 
     function addPostHandler(postData) {
         fetch('http://localhost:8080/posts', {
@@ -25,17 +48,27 @@ function PostList({ isPosting, onStopPosting }) {
                     <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
                 </Modal>
             )}
-            {posts.length > 0 && (
+            {!hasError && !isFetching && posts.length > 0 && (
                 <ul className={classes.posts}>
                     {posts.map((post) => (
                         <Post key={post.key} author={post.author} body={post.body} />
                     ))}
                 </ul>
             )}
-            {posts.length === 0 && (
+            {!hasError && !isFetching && posts.length === 0 && (
                 <div style={{ textAlign: 'center', color: 'white' }}>
                     <h2>There are no posts yet.</h2>
                     <p>Start adding some!</p>
+                </div>
+            )}
+            {!hasError && isFetching && (
+                <div style={{ textAlign: 'center', color: 'white' }}>
+                    <p>Loading posts...</p>
+                </div>
+            )}
+            {hasError && (
+                <div style={{ textAlign: 'center', color: 'white' }}>
+                    <p>Opps, an Error has occurred!</p>
                 </div>
             )}
         </>
