b190b7c5dc1367b322a5ada5e73dc363d2876b49
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-03-14 00:13:50 +0200

    Finished Routing and created NotFound page

--- React/ReactAssignment/yara-react-assignment/src/App.js
+++ React/ReactAssignment/yara-react-assignment/src/App.js
@@ -3,16 +3,23 @@ import Layout from './components/Layout/Layout';
 import './App.css';
 import HomePage from './pages/HomePage/HomePage';
 import Form from './components/FormComponent/Form';
+import NotFound from './pages/NotFoundPage/NotFound';
 
 function App() {
     return (
         <Layout>
             <Switch>
-                <Route path='/' exact >
+                <Route path='/' exact>
                     <HomePage />
                 </Route>
-                <Route path='/form' >
-                    <Form/>
+                <Route path='/form'>
+                    <Form />
+                </Route>
+                <Route path='/table'>
+                    <h1>TEST Table</h1>
+                </Route>
+                <Route path='*'>
+                    <NotFound />
                 </Route>
             </Switch>
         </Layout>
