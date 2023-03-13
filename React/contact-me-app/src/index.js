6bae2a0ce19f111705f337404849eb9bc70a9dfa
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-03-13 14:34:48 +0200

    Added paths with react-router-dom.

--- React/contact-me-app/src/index.js
+++ React/contact-me-app/src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import NewPost from './components/NewPost';
 // import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+  {path: '/', element: <App />},
+  {path: '/create-post', element: <NewPost />}
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
