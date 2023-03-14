615d5ced04235504d5f561a8fac171fb41786b3e
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-03-14 11:48:46 +0200

    Created initial state in form. Styled cancel btn.

--- React/ReactAssignment/yara-react-assignment/src/components/FormComponent/Form.js
+++ React/ReactAssignment/yara-react-assignment/src/components/FormComponent/Form.js
@@ -1,23 +1,29 @@
+import { useState } from 'react';
 import classes from './Form.module.css'
 
 const Form = () => {
+    const [regionInput, setRegionInput] = useState('')
     
+    function regionInputHandler(e) {
+        setRegionInput(e.target.value)
+    }
+
+    function formSubmitHandler(e) {
+        e.preventDefault()
+        console.log(regionInput);
+    }
+
+    function resetHandler() {
+        setRegionInput('')
+    }
 
     return (
-        <form action='#' className={classes.form}>
+        <form className={classes.form} onSubmit={formSubmitHandler}>
             <div>
                 <div className={classes.container}>
                     <label htmlFor='region'>Region</label>
-                    <input type='text' name='region' id='region-input' defaultValue='All' />
+                    <input type='text' name='region' id='region-input' onChange={regionInputHandler} value={regionInput}/>
                 </div>
-                {/* <div className={classes.container}>
-                    <label htmlFor='crop'>Crop</label>
-                    <select name='crop'>
-                        <option>Wheat</option>
-                        <option>Corn</option>
-                        <option>Potato</option>
-                    </select>
-                </div> */}
             </div>
             <div>
                 <label htmlFor='calendar-name'>Calendar name</label>
@@ -29,9 +35,12 @@ const Form = () => {
             <div>
                 <label htmlFor='variety'>Please specify the crop variety:</label>
                 <input type='text' name='variety' />
-                <div>
-                    <button className='btn' button='submit'>
-                        Submit
+                <div className={classes.btnContainer}>
+                <button className='btnCancel' type='button' onClick={resetHandler}>
+                        Cancel
+                    </button>
+                    <button className='btn'>
+                        Save
                     </button>
                 </div>
             </div>
