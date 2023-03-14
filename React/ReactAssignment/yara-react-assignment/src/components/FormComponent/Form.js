4c27c1fe73596c7d5a8bf0ab70ecd018b84456ee
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-03-14 15:57:17 +0200

    Finish initial form validation and validation styling

--- React/ReactAssignment/yara-react-assignment/src/components/FormComponent/Form.js
+++ React/ReactAssignment/yara-react-assignment/src/components/FormComponent/Form.js
@@ -36,6 +36,9 @@ const Form = () => {
         console.log(regionInput);
         console.log(calendarName);
         console.log(varietyInput);
+        setRegionInputError(false);
+        setCalendarError(false);
+        setVarietyInput(false);
     }
 
     function resetHandler() {
@@ -44,30 +47,44 @@ const Form = () => {
         setVarietyInput('');
     }
 
+    const regionInputClass = !regionHasError ? classes.input : classes.error;
+    const calendarInputClass = !calendarHasError ? classes.input : classes.error;
+    const varietyInputClass = !varietyHasError ? classes.input : classes.error;
+
     return (
         <form className={classes.form} onSubmit={formSubmitHandler}>
             <div>
                 <div className={classes.container}>
                     <label htmlFor='region'>Region</label>
                     <input
+                        className={regionInputClass}
                         type='text'
                         name='region'
                         id='region-input'
                         onChange={regionInputHandler}
                         value={regionInput}
                     />
+                    {regionHasError && <p className={classes.errorText}>Input is empty</p>}
                 </div>
             </div>
             <div className={classes.container}>
                 <label htmlFor='calendar-name'>Calendar name</label>
-                <input type='text' name='calendarName' onChange={calendarNameHandler} value={calendarName} />
+                <input
+                    className={calendarInputClass}
+                    type='text'
+                    name='calendarName'
+                    onChange={calendarNameHandler}
+                    value={calendarName}
+                />
+                {calendarHasError && <p className={classes.errorText}>Input is empty</p>}
                 <p id='max-char'>
                     Optional description but not including cultivation type, region & varieties. (50 characters)
                 </p>
             </div>
             <div className={classes.container}>
                 <label htmlFor='variety'>Please specify the crop variety:</label>
-                <input type='text' name='variety' onChange={varietyInpytHandler} value={varietyInput} />
+                <input className={varietyInputClass} type='text' name='variety' onChange={varietyInpytHandler} value={varietyInput} />
+                {varietyHasError && <p className={classes.errorText}>Input is empty</p>}
             </div>
             <div className={classes.btnContainer}>
                 <button className='btnCancel' type='button' onClick={resetHandler}>
