c943f09237d92e0f24c7e1c11df31704085f26ba
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-03-16 21:44:20 +0200

    Created initial SearchBar component with onSubmit props

--- React/PicSearcherApp/src/api.js
+++ React/PicSearcherApp/src/api.js
@@ -1,18 +1,19 @@
 import axios from 'axios';
+import { accessKey } from './components/credentials/accessKey';
 
-const searchImages = async () => {
+const searchImages = async (term) => {
   const response = await axios.get('https://api.unsplash.com/search/photos', {
     headers: {
-      Authorization: 'Client-ID 8O50V7bNzfKdVixwS9W9nZVdr0VnrCv9gmeimfdvp6Y',
+      Authorization: `Client-ID ${accessKey}`,
     },
     params: {
-      query: 'cars',
+      query: term,
     },
   });
 
-  console.log(response);
+  console.log(response.data.results);
 
-  return response;
+  return response.data.results;
 };
 
 export default searchImages;
