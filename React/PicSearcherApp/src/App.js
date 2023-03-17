4cde1b43fc53cf88c9f3b1789b367d504b34eb0a
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-03-17 10:42:43 +0200

    Refactored api.js to use async await

--- React/PicSearcherApp/src/App.js
+++ React/PicSearcherApp/src/App.js
@@ -2,9 +2,13 @@ import searchImages from './api';
 import SearchBar from './components/SearchBar';
 
 function App() {
-  const handleSubmit = (term) => {
-    searchImages(term)
+
+  const handleSubmit = async (term) => {
+    const result = await searchImages(term)
+
+    console.log(result);
   }
+
     return (
         <div>
             <SearchBar onSubmit={handleSubmit}/>
