9cb5a9f118b92357f8f0c56d7a8250d3573c9cf5
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-03-19 00:31:45 +0200

    Created BookList component to map all books to BookShow components

--- React/BookReading-App/src/App.js
+++ React/BookReading-App/src/App.js
@@ -1,17 +1,19 @@
 import { useState } from 'react';
 import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 
 function App() {
     const [books, setBooks] = useState([]);
 
     function createBookHandler(title) {
-        const updatedBooks = [...books, { id: Math.random(), title }];
+        const updatedBooks = [...books, { id: Math.round(Math.random() * 10000), title }];
         setBooks(updatedBooks);
     }
 
     return (
-        <div>
-            <BookCreate onCreate={createBookHandler} />{books.length}
+        <div className='app'>
+            <BookList books={books} />
+            <BookCreate onCreate={createBookHandler} />
         </div>
     );
 }
--- React/BookReading-App/src/components/BookList.js
+++ React/BookReading-App/src/components/BookList.js
@@ -1,5 +1,8 @@
-function BookList() {
-  return <div>BookList</div>;
+import BookShow from "./BookShow";
+
+function BookList({ books }) {
+
+    return <div className="book-list">{books.map(book => <BookShow key={book.id} book={book}/>)}</div>;
 }
 
 export default BookList;
