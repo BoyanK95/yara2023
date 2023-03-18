bcbd7c9e1a1c7995027383328e0daace546266c1
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-03-19 00:18:23 +0200

    Created createBookHandler for setting updatedBooks

--- React/BookReading-App/src/App.js
+++ React/BookReading-App/src/App.js
@@ -1,14 +1,19 @@
-import { useState } from "react";
-import BookCreate from "./components/BookCreate";
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
 
 function App() {
-  const [books, setBooks] = useState([])
+    const [books, setBooks] = useState([]);
 
-  function createBookHandler(title) {
-    console.log('Need to add book with: ', title);
-  }
+    function createBookHandler(title) {
+        const updatedBooks = [...books, { id: Math.random(), title }];
+        setBooks(updatedBooks);
+    }
 
-  return <div><BookCreate onCreate={createBookHandler} /></div>;
+    return (
+        <div>
+            <BookCreate onCreate={createBookHandler} />{books.length}
+        </div>
+    );
 }
 
 export default App;
