b5186e2a45d1de4763f262cf0aeaab5cfe577b52
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-03-18 20:49:37 +0200

    Created bookCreate component

--- React/BookReading-App/src/App.js
+++ React/BookReading-App/src/App.js
@@ -1,5 +1,14 @@
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+
 function App() {
-  return <div>App</div>;
+  const [books, setBooks] = useState([])
+
+  function createBookHandler(title) {
+    console.log('Need to add book with: ', title);
+  }
+
+  return <div><BookCreate onCreate={createBookHandler} /></div>;
 }
 
 export default App;
