c3bd55f11f3de60190d733a324487fc5041ed681
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-03-20 12:25:34 +0200

    Created Edit button, starting work on edit functionality

--- React/BookReading-App/src/components/BookShow.js
+++ React/BookReading-App/src/components/BookShow.js
@@ -1,13 +1,23 @@
+import { useState } from 'react';
+import BookEdit from './BookEdit';
+
 function BookShow({ book, onDelete }) {
-  
+    const [showEdit, setShowEdit] = useState(false);
+
     function deleteHandler() {
         onDelete(book.id);
+    }
+    function toggleEdit() {
+        setShowEdit(!showEdit);
     }
 
     return (
         <div className='book-show'>
-            {book.title}
+            <div>{showEdit ? <BookEdit /> : <h3>{book.title}</h3>}</div>
             <div className='actions'>
+                <button className='edit' onClick={toggleEdit}>
+                    Edit
+                </button>
                 <button className='delete' onClick={deleteHandler}>
                     Delete
                 </button>
