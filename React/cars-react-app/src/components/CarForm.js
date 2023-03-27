0b7bca3b958409d3b1cdbf3d2a445b01d8256649
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-03-27 10:57:37 +0300

    Created carForm component and connected it to formReducer

--- React/cars-react-app/src/components/CarForm.js
+++ React/cars-react-app/src/components/CarForm.js
@@ -1,5 +1,30 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { changeName } from '../store/slice/formSlice';
+
 function CarForm() {
-    return <div>CarForm</div>;
+    const dispatch = useDispatch();
+
+    const name = useSelector((state) => {
+        return state.form.name;
+    });
+
+    const handleNameChange = (e) => {
+        dispatch(changeName(e.target.value));
+    };
+
+    return (
+        <div className='car-form panel'>
+            <h4>Add Car</h4>
+            <form>
+                <div className='field-group'>
+                    <div>
+                        <label className='label'>Name</label>
+                        <input className='input is-expanded' value={name} onChange={handleNameChange} />
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
 }
 
-export default CarForm
+export default CarForm;
