ba4f4f1e790ea8870d5920ea00f4ac209eaf001d
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-03-28 09:32:56 +0300

    Created Search and Total Cost functionality

--- React/cars-react-app/src/components/CarList.js
+++ React/cars-react-app/src/components/CarList.js
@@ -3,12 +3,15 @@ import { removeCar } from '../store';
 
 function CarList() {
     const dispatch = useDispatch();
-    const cars = useSelector((state) => {
-        return state.cars.data;
+
+    const cars = useSelector(({ cars: { data, searchTerm } }) => {
+        return data.filter((car) =>
+          car.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
     });
 
     function handleCarDelete(car) {
-        dispatch(removeCar(car.id))
+        dispatch(removeCar(car.id));
     }
 
     const renderedCars = cars.map((car) => {
