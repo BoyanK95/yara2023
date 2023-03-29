1ae43d9cfd7cf564c7ae073f3baf7b8c1ab195be
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-03-29 22:43:46 +0300

    Added addGoalHandler functionality

--- ReactNative/my-first-react-native-app/App.js
+++ ReactNative/my-first-react-native-app/App.js
@@ -1,33 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your course goal!'/>
-        <Button title='Add goal'/>
-      </View>
-      <View>
-        <Text>List of goals...</Text>
-      </View>
-    </View>
-  );
+    const [enteredGoalText, setEnteredGoalText] = useState('');
+    const [courseGoals, setCourseGoals] = useState([]);
+
+    function goalnputHandler(enteredTest) {
+        setEnteredGoalText(enteredTest);
+    }
+
+    function addGoalHandler() {
+        setCourseGoals((currentCourseGoals) => [...currentCourseGoals, enteredGoalText]);
+    }
+
+    return (
+        <View style={styles.appContainer}>
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.textInput} placeholder='Your course goal!' onChangeText={goalnputHandler} />
+                <Button title='Add goal' onPress={addGoalHandler} />
+            </View>
+            <View style={styles}>
+                {courseGoals.map(goal => <Text key={goal}>{goal}</Text>)}
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  appContainer: {
-    padding: 50
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: 'darkgray',
-    width: '80%',
-    marginRight: 8,
-    padding: 8
-  }
+    appContainer: {
+        padding: 50
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    textInput: {
+        borderWidth: 1,
+        borderColor: 'darkgray',
+        width: '80%',
+        marginRight: 8,
+        padding: 8
+    }
 });
