2ade0dc13b722b6f4ea2b28b8d0dce5267c50f13
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-04-02 22:05:18 +0300

    Refactored GoalInput

--- ReactNative/my-first-react-native-app/GoalInput.js
+++ ReactNative/my-first-react-native-app/GoalInput.js
@@ -1,38 +1,48 @@
 import { useState } from 'react';
-import { TextInput, View, StyleSheet, Button } from 'react-native';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
 
 function GoalInput(props) {
-    const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [enteredGoalText, setEnteredGoalText] = useState('');
 
-    function goalnputHandler(enteredTest) {
-        setEnteredGoalText(enteredTest);
-    }
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
 
-    return (
-        <View style={styles.inputContainer}>
-            <TextInput
-                style={styles.textInput}
-                value={enteredGoalText}
-                placeholder='Set your goals here!'
-                onChangeText={goalnputHandler}
-            />
-            <Button title='Add Goal' onPress={props.onAddGoal} />
-        </View>
-    );
+  function addGoalHandler() {
+    props.onAddGoal(enteredGoalText);
+    setEnteredGoalText('');
+  }
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Your course goal!"
+        onChangeText={goalInputHandler}
+        value={enteredGoalText}
+      />
+      <Button title="Add Goal" onPress={addGoalHandler} />
+    </View>
+  );
 }
 
 export default GoalInput;
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    },
-    textInput: {
-        borderWidth: 1,
-        borderColor: 'darkgray',
-        width: '80%',
-        marginRight: 8,
-        padding: 8
-    }
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8,
+  },
 });
