373d7815de80eb18cb9b296a9638f1512c359279
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-04-03 22:40:55 +0300

    Added Image and last styles to GameOverScreen

--- ReactNative/number-guesser-app/screens/GameOverScreen.js
+++ ReactNative/number-guesser-app/screens/GameOverScreen.js
@@ -1,7 +1,37 @@
-import { Text } from "react-native"
+import { Image, StyleSheet, Text, View } from 'react-native';
+import Title from '../components/Title';
 
 function GameOverScreen(params) {
-    return <Text>Game is Over</Text>
+    return (
+        <View style={styles.rootContainer}>
+            <Title>Game Over!</Title>
+            <View style={styles.imageContainer}>
+                <Image style={styles.image} source={require('../assets/images/endGoal.png')} />
+            </View>
+        </View>
+    );
 }
 
-export default GameOverScreen
+export default GameOverScreen;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        padding: 24,
+        justifyContent: 'center',
+        alignContent: 'center'
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: 'darkblue',
+        overflow: 'hidden',
+        margin: 36
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    }
+});
