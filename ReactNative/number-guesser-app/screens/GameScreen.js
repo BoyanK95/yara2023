b451cb6f46d57167c602f87f38207d8263e3b649
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-04-03 16:03:19 +0300

    Created Validation to chek if lieing

--- ReactNative/number-guesser-app/screens/GameScreen.js
+++ ReactNative/number-guesser-app/screens/GameScreen.js
@@ -1,12 +1,12 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Alert } from 'react-native';
 import Title from '../components/Title';
 import { generateRandomBetween } from '../helpers/generateRandomNum';
 import { useState } from 'react';
 import NumberContainer from '../components/game/NumberContainer';
 import PrimaryButton from '../components/PrimaryButton';
 
-let minNum = 1
-let maxNum = 100
+let minNum = 1;
+let maxNum = 100;
 
 function GameScreen({ userNumber }) {
     const initialGuess = generateRandomBetween(minNum, maxNum, userNumber);
@@ -14,13 +14,22 @@ function GameScreen({ userNumber }) {
     const [currentGuess, setCurrentGuess] = useState(initialGuess);
 
     function nextGuessHandler(direction) {
-        if (direction === 'lower') {
-            maxNum = currentGuess
-        } else {
-            minNum = currentGuess + 1
+        if (
+            (direction === 'lower' && currentGuess < userNumber) ||
+            (direction === 'greater' && currentGuess > userNumber)
+        ) {
+            Alert.alert("Don't lie!!!", 'You know that this is wrong...', [
+                {text: 'Sorry', style: 'cancel'}
+            ])
+            return
         }
-        const newRandomNum = generateRandomBetween(minNum, maxNum, currentGuess)
-        setCurrentGuess(newRandomNum)
+        if (direction === 'lower') {
+            maxNum = currentGuess;
+        } else {
+            minNum = currentGuess + 1;
+        }
+        const newRandomNum = generateRandomBetween(minNum, maxNum, currentGuess);
+        setCurrentGuess(newRandomNum);
     }
 
     return (
