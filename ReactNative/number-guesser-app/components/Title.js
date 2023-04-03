156abbf79d10d72ee85c6fe96ff9e282c9c217d8
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-04-03 22:23:03 +0300

    Added Title to StartGameScreen

--- ReactNative/number-guesser-app/components/Title.js
+++ ReactNative/number-guesser-app/components/Title.js
@@ -14,6 +14,7 @@ const styles = StyleSheet.create({
         color: 'green',
         textAlign: 'center',
         borderColor: 'green',
+        borderWidth: 2,
         padding: 12
     }
 })
--- ReactNative/number-guesser-app/screens/StartGameScreen.js
+++ ReactNative/number-guesser-app/screens/StartGameScreen.js
@@ -1,8 +1,9 @@
-import { TextInput, View, StyleSheet, Alert } from 'react-native';
+import { TextInput, View, Text, StyleSheet, Alert } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 import { useState } from 'react';
+import Title from '../components/Title';
 
-function StartGameScreen({onPickedNumber}) {
+function StartGameScreen({ onPickedNumber }) {
     const [inputNumber, setInputNumber] = useState('');
 
     function numberInputHanlder(input) {
@@ -22,26 +23,30 @@ function StartGameScreen({onPickedNumber}) {
             ]);
             return;
         }
-        onPickedNumber(chosenNumber)
+        onPickedNumber(chosenNumber);
     }
 
     return (
-        <View style={styles.inputContainer}>
-            <TextInput
-                style={styles.numberInput}
-                maxLength={2}
-                keyboardType='number-pad'
-                autoCapitalize='none'
-                autoCorrect={false}
-                onChangeText={numberInputHanlder}
-                value={inputNumber}
-            />
-            <View style={styles.buttonsContainer}>
-                <View style={styles.btnContainer}>
-                    <PrimaryButton onPress={resetHandler}>Reset</PrimaryButton>
-                </View>
-                <View style={styles.btnContainer}>
-                    <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+        <View style={styles.rooContainer}>
+            <Title>Guess My Number</Title>
+            <View style={styles.inputContainer}>
+                <Text style={styles.instructionText}>Enter a number</Text>
+                <TextInput
+                    style={styles.numberInput}
+                    maxLength={2}
+                    keyboardType='number-pad'
+                    autoCapitalize='none'
+                    autoCorrect={false}
+                    onChangeText={numberInputHanlder}
+                    value={inputNumber}
+                />
+                <View style={styles.buttonsContainer}>
+                    <View style={styles.btnContainer}>
+                        <PrimaryButton onPress={resetHandler}>Reset</PrimaryButton>
+                    </View>
+                    <View style={styles.btnContainer}>
+                        <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+                    </View>
                 </View>
             </View>
         </View>
@@ -51,10 +56,15 @@ function StartGameScreen({onPickedNumber}) {
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
+    rooContainer: {
+        flex: 1,
+        marginTop: 100,
+        alignItems: 'center'
+    },
     inputContainer: {
         justifyContent: 'center',
         alignItems: 'center',
-        marginTop: 100,
+        marginTop: 33,
         marginHorizontal: 24,
         padding: 16,
         backgroundColor: '#065607',
@@ -64,6 +74,11 @@ const styles = StyleSheet.create({
         shadowOffset: { width: 3, height: 3 },
         shadowRadius: 6,
         shadowOpacity: 0.25
+    },
+    instructionText: {
+        color: '#5cf85c',
+        fontSize: 23,
+        fontWeight: 'bold'
     },
     numberInput: {
         height: 50,
