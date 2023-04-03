4825cc23972b26e0c4cc007d89f1a780118d9bc1
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-04-03 03:43:59 +0300

    Added styles to StarGameScreen and  PrimaryButton

--- ReactNative/number-guesser-app/components/PrimaryButton.js
+++ ReactNative/number-guesser-app/components/PrimaryButton.js
@@ -1,11 +1,44 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
 function PrimaryButton({ children }) {
+    function pressHandler() {
+        
+    }
+
     return (
-        <View>
-            <Text>{children}</Text>
+        <View style={styles.btnOuterContainer}>
+            <Pressable
+                style={({ pressed }) =>
+                    pressed ? [styles.btnInnercontainer, styles.pressed] : styles.btnInnercontainer
+                }
+                onPress={pressHandler}
+                android_ripple={{ color: '#086422' }}
+            >
+                <Text style={styles.buttonText}>{children}</Text>
+            </Pressable>
         </View>
     );
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+    btnOuterContainer: {
+        borderRadius: 28,
+        margin: 4,
+        overflow: 'hidden'
+    },
+    btnInnercontainer: {
+        backgroundColor: '#0d8e31',
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center'
+    },
+    pressed: {
+        opacity: 0.75
+    }
+});
